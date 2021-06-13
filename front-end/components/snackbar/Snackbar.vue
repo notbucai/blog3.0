f919edd2c80a6d4081691c6f057107e53129d33f
bucai <1450941858@qq.com>
2021-06-13 11:21:58 +0800

    fix toast and feat toast color

--- front-end/components/snackbar/Snackbar.vue
+++ front-end/components/snackbar/Snackbar.vue
@@ -2,31 +2,43 @@
  * @Author: bucai
  * @Date: 2020-05-03 12:11:12
  * @LastEditors: bucai
- * @LastEditTime: 2020-05-03 12:51:47
+ * @LastEditTime: 2021-06-13 11:19:35
  * @Description: 
  -->
 <template>
-  <v-snackbar v-model="open" :color="color" :timeout="timeout" top>{{ message }}</v-snackbar>
+  <v-snackbar v-model="open" :color="color" :timeout="timeout" top>{{
+    message
+  }}</v-snackbar>
 </template>
  
 <script>
 export default {
   name: 'Snackbar',
-  data() {
+  data () {
     return {
       icon: '',
-      color: '',
+      type: '',
       open: false,
       message: '',
       timeout: 1500
     };
   },
-  created() {},
+  computed: {
+    color () {
+      const c = {
+        'success': '#67c23a',
+        'warning': '#e6a23c',
+        'info': '#909399',
+        'error': '#f56c6c'
+      };
+      return c[this.type]
+    }
+  },
+  created () { },
   methods: {
   }
 };
 </script>
  
 <style lang='scss'>
-
 </style>

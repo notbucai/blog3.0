44272f846a7ea7995df0edc4d087c5dcaf6645b8
bucai <1450941858@qq.com>
2022-07-30 13:31:57 +0800

    fix: 修复状态

--- server/src/modules/transfer/transfer.controller.ts
+++ server/src/modules/transfer/transfer.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Roles } from '../../core/decorators/roles.decorator';
+import { CurUser } from '../../core/decorators/user.decorator';
 import { ActiveGuard } from '../../core/guards/active.guard';
 import { RolesGuard } from '../../core/guards/roles.guard';
+import { User } from '../../entities/User';
 import { TransferService } from './transfer.service';
 
 @Controller('transfer')
@@ -15,9 +17,10 @@ export class TransferController {
 
   @Get('/run')
   @ApiBearerAuth()
-  @UseGuards(ActiveGuard, RolesGuard)
-  @Roles('transfer-tool', true)
-  async run() {
+  @UseGuards(ActiveGuard)
+  // @Roles('transfer-tool', true)
+  async run(@CurUser() user: User) {
+    if (!user.isAdmin) return;
     return this.transferService.run();
   }
 
